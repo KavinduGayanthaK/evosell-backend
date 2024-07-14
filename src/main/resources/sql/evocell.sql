2b86a6a66d3057ba7916fdf30fc67d14eafb6455
Kavindu <gayanthakavindu3@gmail.com>
2024-07-14 11:56:02 +0530

    customer table created

--- src/main/resources/sql/evocell.sql
+++ src/main/resources/sql/evocell.sql
@@ -1,2 +1,10 @@
 CREATE DATABASE evosell;
 USE evosell;
+
+CREATE TABLE customer(
+    id VARCHAR(10) PRIMARY KEY ,
+    name VARCHAR(50) NOT NULL ,
+    address VARCHAR(100) NOT NULL ,
+    email VARCHAR(50),
+    nic VARCHAR(20) NOT NULL
+);
