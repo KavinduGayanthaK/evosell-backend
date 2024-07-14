3ea8a7ea09b1225414037073f92354a092ea5f4c
Kavindu <gayanthakavindu3@gmail.com>
2024-07-14 12:05:36 +0530

    orders table created

--- src/main/resources/sql/evocell.sql
+++ src/main/resources/sql/evocell.sql
@@ -8,3 +8,9 @@ CREATE TABLE customer(
     email VARCHAR(50),
     nic VARCHAR(20) NOT NULL
 );
+
+CREATE TABLE orders(
+    id VARCHAR(10) PRIMARY KEY ,
+    customerId VARCHAR(10) NOT NULL,
+    customerName VARCHAR(50) NOT NULL
+)
