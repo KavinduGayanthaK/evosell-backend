8cc3cca8c12f18d79f6c2ef4c08ffa50f4175bbb
Kavindu <gayanthakavindu3@gmail.com>
2024-07-14 12:39:03 +0530

    created orderDetails table

--- src/main/resources/sql/evocell.sql
+++ src/main/resources/sql/evocell.sql
@@ -19,6 +19,19 @@ CREATE TABLE orders(
 CREATE TABLE item(
     itemCode VARCHAR(10) PRIMARY KEY ,
     name VARCHAR(50) NOT NULL ,
-    quantityOnHand INT(10) NOT NULL ,
+    quantityOnHand INT NOT NULL ,
     price DOUBLE(5,2) NOT NULL
 );
+
+CREATE TABLE orderDetails(
+    orderId VARCHAR(10) NOT NULL ,
+    itemCode varchar(10) NOT NULL ,
+    itemName VARCHAR(50) NOT NULL ,
+    qty INT NOT NULL ,
+    total DOUBLE(8,2) NOT NULL ,
+    discount DOUBLE(5,2) NOT NULL ,
+    netTotal DOUBLE(8,2) NOT NULL ,
+    date DATE,
+    FOREIGN KEY(orderId) REFERENCES orders(id),
+    FOREIGN KEY(itemCode) REFERENCES item(itemCode)
+)
