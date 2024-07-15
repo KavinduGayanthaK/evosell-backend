fd600be190f5c6a2e7a67f15046c75bbe68397b5
Kavindu <gayanthakavindu3@gmail.com>
2024-07-15 15:58:30 +0530

    change id field length

--- src/main/resources/sql/evocell.sql
+++ src/main/resources/sql/evocell.sql
@@ -1,30 +1,31 @@
+DROP DATABASE evosell;
 CREATE DATABASE evosell;
 USE evosell;
 
 CREATE TABLE customer(
-    id VARCHAR(10) PRIMARY KEY ,
+    id VARCHAR(500) PRIMARY KEY ,
     name VARCHAR(50) NOT NULL ,
-    address VARCHAR(100) NOT NULL ,
+    address VARCHAR(200) NOT NULL ,
     email VARCHAR(50),
     nic VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE orders(
-    id VARCHAR(10) PRIMARY KEY ,
+    id VARCHAR(250) PRIMARY KEY ,
     customerId VARCHAR(10) NOT NULL,
     customerName VARCHAR(50) NOT NULL,
     FOREIGN KEY (customerId) REFERENCES customer(id)
 );
 
 CREATE TABLE item(
-    itemCode VARCHAR(10) PRIMARY KEY ,
+    itemCode VARCHAR(250) PRIMARY KEY ,
     name VARCHAR(50) NOT NULL ,
     quantityOnHand INT NOT NULL ,
     price DOUBLE(5,2) NOT NULL
 );
 
 CREATE TABLE orderDetails(
-    orderId VARCHAR(10) NOT NULL ,
+    orderId VARCHAR(250) NOT NULL ,
     itemCode varchar(10) NOT NULL ,
     itemName VARCHAR(50) NOT NULL ,
     qty INT NOT NULL ,
