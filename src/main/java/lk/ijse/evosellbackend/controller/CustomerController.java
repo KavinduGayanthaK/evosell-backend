550eb852c0df2a5dd85eb18cd959a8d5381e5b61
Kavindu <gayanthakavindu3@gmail.com>
2024-07-15 10:42:35 +0530

    created init method and db connection

--- src/main/java/lk/ijse/evosellbackend/controller/CustomerController.java
+++ src/main/java/lk/ijse/evosellbackend/controller/CustomerController.java
@@ -1,4 +1,29 @@
 package lk.ijse.evosellbackend.controller;
 
-public class CustomerController {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@WebServlet(value = "/customer")
+public class CustomerController extends HttpServlet {
+
+    Connection connection;
+
+    @Override
+    public void init() throws ServletException {
+        try {
+            var driverClass = getServletContext().getInitParameter("driver-class");
+            var dbUrl = getServletContext().getInitParameter("dbURL");
+            var dbUserName = getServletContext().getInitParameter("dbPassword");
+            var dbPassword = getServletContext().getInitParameter("dbUserName");
+            Class.forName(driverClass);
+            this.connection = DriverManager.getConnection(dbUrl,dbUserName,dbPassword);
+        }catch (ClassNotFoundException | SQLException e){
+            e.printStackTrace();;
+        }
+    }
 }
