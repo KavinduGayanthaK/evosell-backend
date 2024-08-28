b311ce282356e12ae84cbf56bea5f2bc6071190e
Kavindu <gayanthakavindu3@gmail.com>
2024-08-28 12:21:42 +0530

    crete item controller

---
+++ src/main/java/lk/ijse/evosellbackend/controller/ItemController.java
@@ -0,0 +1,12 @@
+package lk.ijse.evosellbackend.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/item")
+public class ItemController extends HttpServlet {
+
+
+
+
+}
