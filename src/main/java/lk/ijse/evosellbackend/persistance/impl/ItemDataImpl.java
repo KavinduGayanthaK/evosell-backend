3772a7cb92d5b8ecf3ed639f28599ab14b7e6c6f
Kavindu <gayanthakavindu3@gmail.com>
2024-08-28 12:46:33 +0530

    crete itemDataImpl class

---
+++ src/main/java/lk/ijse/evosellbackend/persistance/impl/ItemDataImpl.java
@@ -0,0 +1,19 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package lk.ijse.evosellbackend.persistance.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import lk.ijse.evosellbackend.dto.ItemDTO;
+import lk.ijse.evosellbackend.persistance.ItemData;
+
+public class ItemDataImpl implements ItemData {
+
+}
